7da9533626ef45daf9c91dec3e19ba349fd72767
Loan <loan.jean@ens.uvsq.fr>
2026-05-11 13:51:24 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const targetUrl = 'https://mmi.place'
+
+if (typeof window !== 'undefined' && !window.location.href.startsWith(targetUrl)) {
+	window.location.replace(targetUrl)
+}
+
 const app = createApp(App)
 
 app.use(router)
